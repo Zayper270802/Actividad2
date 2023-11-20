8c8d692b60ceb2b950dca524c2ac62bde8a2d32a
Zayper270802 <zaycabrera2708@gmail.com>
2023-11-20 14:51:56 -0600

    first commit

---
+++ Ejercicio01/scripts/holamundo.js
@@ -0,0 +1,9 @@
+
+
+function imprime(mensaje, veces) {
+    for (var i = 0; i < veces; i++) {
+        console.log(mensaje);
+    }
+}
+
+imprime("Hola mundo", 4);
---
+++ Ejercicio03/script.js
@@ -0,0 +1,7 @@
+function abrirRegalo() {
+    const image = document.querySelector('img');
+    image.src = 'giphy.gif';
+}
+
+const image = document.querySelector('img');
+image.addEventListener('click', abrirRegalo);
